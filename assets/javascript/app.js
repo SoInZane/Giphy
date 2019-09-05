0c6800569abee7e16df558ed6b8730892dafb227
SoInZane <51385126+SoInZane@users.noreply.github.com>
2019-09-04 23:57:19 -0500

    corrected syntax in js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,18 @@
 //array of superheroes to start with
 var superHeroes = [
     "batman", "superman", "ironman", "captain america",
-    "spiderman", "barry allen", "hulk", "wolverine", 
+    "spiderman", "barry allen", "hulk", "wolverine",
     "thor", "scott summers", "jean grey"
 ]
 
 //function to display the buttons with the above names of superheroes
-function showButtons(){
+function showButtons() {
 
     $("#buttons").empty();
 
     for (var i = 0; i < superHeroes.length; i++) {
         $("#buttons").append(`<button classe="btn">${superHeroes[i]}`);
-}
+    }
 }
 
 showButtons();
@@ -34,9 +34,10 @@ $(document).on('click', "button", function () {
 
         .then(function (response) {
             console.log(response);
-//loop through the array of superheros and after pulling down data from the api display it in the html
+            //loop through the array of superheros and after pulling down data from the api display it in the html
             for (var i = 0; i < response.data.length; i++) {
                 var result = response.data[i];
+
                 $('#gifsSuperheroes').prepend("<p>Rating: " + response.data[i].rating + "</p>");
                 $('#gifsSuperheroes').prepend("<img src='" + response.data[i].images.fixed_height.url + "'>");
             }
@@ -45,14 +46,14 @@ $(document).on('click', "button", function () {
 
 //on click of the submit button add a new superhero to the array and add a new button
 
-$(document).on("click", "#submitButton", function(event){
+$(document).on("click", "#submitButton", function (event) {
     event.preventDefault();
-  
+
     // adds new superhero to the superHeroes array
     var userEntry = $("#newSuperhero").val();
     superHeroes.push(userEntry);
     console.log(userEntry);
-    
-    // rebuilds all buttons with the new array
+
+    // rebuilds all buttons with the new entry in the arrary added
     showButtons();
-  })
+})
