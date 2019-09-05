289e942d823ab609111f53b66677308fb49b98d3
SoInZane <51385126+SoInZane@users.noreply.github.com>
2019-09-04 23:31:08 -0500

    removed static buttons from html, rewrote pieces of js to dynamically add the buttons from an array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,58 @@
-$('button').on('click',function(){
-            
-    var sh = $(this).data("search");
+//array of superheroes to start with
+var superHeroes = [
+    "batman", "superman", "ironman", "captain america",
+    "spiderman", "barry allen", "hulk", "wolverine", 
+    "thor", "scott summers", "jean grey"
+]
+
+//function to display the buttons with the above names of superheroes
+function showButtons(){
+
+    $("#buttons").empty();
+
+    for (var i = 0; i < superHeroes.length; i++) {
+        $("#buttons").append(`<button classe="btn">${superHeroes[i]}`);
+}
+}
+
+showButtons();
+
+//on click query the api and search based off of name of superhero
+$(document).on('click', "button", function () {
+
+    var sh = $(this).text();
     console.log(sh);
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q="+sh+"&api_key=YAoar8vnGWqmr4eBqYMAbjHsKKIVTgCz&limit=10";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + sh + "&api_key=YAoar8vnGWqmr4eBqYMAbjHsKKIVTgCz&limit=10";
     console.log(queryURL);
-    $.ajax({url:queryURL,method:'GET'})
-    .done(function(response){
-        console.log(response);
-        for(var i=0;i<response.data.length;i++){
-            $('#gifsSuperheroes').prepend("<p>Rating: "+response.data[i].rating+"</p>");
-            $('#gifsSuperheroes').prepend("<img src='"+response.data[i].images.downsized.url+"'>");
-        }
+
+
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
     })
-})
+
+        .then(function (response) {
+            console.log(response);
+//loop through the array of superheros and after pulling down data from the api display it in the html
+            for (var i = 0; i < response.data.length; i++) {
+                var result = response.data[i];
+                $('#gifsSuperheroes').prepend("<p>Rating: " + response.data[i].rating + "</p>");
+                $('#gifsSuperheroes').prepend("<img src='" + response.data[i].images.fixed_height.url + "'>");
+            }
+        })
+});
+
+//on click of the submit button add a new superhero to the array and add a new button
+
+$(document).on("click", "#submitButton", function(event){
+    event.preventDefault();
+  
+    // adds new superhero to the superHeroes array
+    var userEntry = $("#newSuperhero").val();
+    superHeroes.push(userEntry);
+    console.log(userEntry);
+    
+    // rebuilds all buttons with the new array
+    showButtons();
+  })
